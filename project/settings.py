e53b6890d09f1deae5f6306056f6e7c5a309384a
Islam412 <islam.124.hamdy@gmail.com>
2023-10-12 01:35:45 +0300

    add postgespostgres sql

--- project/settings.py
+++ project/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mycoffeedb',
+        'USER': 'postgres',
+        'PASSWORD': '112233456',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
