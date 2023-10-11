227541e74a6d7ff84e9b473652c438fcbaa526cc
Islam412 <islam.124.hamdy@gmail.com>
2023-10-11 03:17:27 +0300

    add static files

--- pages/views.py
+++ pages/views.py
@@ -1,14 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
-
 
 def index(request):
-    return HttpResponse('<h1>Home page</h1>')
+    return render(request , 'pages/index.html')
 
 
 def about(request):
-    return HttpResponse('<h1>About pages</h1>')
-
-# def coffee(request):
-#     return HttpResponse('<h1>Coffee shop</h1>')
+    return render(request , 'pages/about.html')
