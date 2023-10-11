7691c50d12040ec72e217e147fac28df2bae9ce0
Islam412 <islam.124.hamdy@gmail.com>
2023-10-11 05:41:22 +0300

    add product app

--- project/urls.py
+++ project/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('pages.urls')),
     path('accounts/',include('accounts.urls')),
-    path('products/',include('products/urls.py')),
+    path('products/',include('products.urls')),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
