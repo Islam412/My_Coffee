65287201a6ac14a107e2b390723d5e1257cb59d0
Islam412 <islam.124.hamdy@gmail.com>
2023-10-11 06:06:37 +0300

    add product.html

--- products/views.py
+++ products/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 
 def products(request):
     return render(request,'products/products.html')
+
+
+def product(request):
+    return render(request,'products/product.html') 
