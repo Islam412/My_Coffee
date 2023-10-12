15aa6fbfb1d336ed9902dbfd174f59dc98f61ed5
Islam412 <islam.124.hamdy@gmail.com>
2023-10-13 02:25:17 +0300

    desing models product party 1

--- products/models.py
+++ products/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from datetime import datetime
 # Create your models here.
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(max_length=20000)
+    price = models.DecimalField(max_digits=6 , decimal_places=2)
+    photo = models.ImageField('photos/%y%m%d')
+    is_active = models.BooleanField(default=True)
+    publish_date = models.DateTimeField(default=datetime.now)
+
+    def __str__(self):
+        return self.name
