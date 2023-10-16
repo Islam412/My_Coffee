7df25d0effb632fa519a61dc311988f768206ed2
Islam412 <islam.124.hamdy@gmail.com>
2023-10-16 03:17:27 +0300

    Run search by name and description

--- products/models.py
+++ products/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from datetime import datetime
-# Create your models here.
 
 
 class Product(models.Model):
--- products/views.py
+++ products/views.py
@@ -1,21 +1,32 @@
 from django.shortcuts import render,get_object_or_404
 from .models import Product
 
-# Create your views here.
 
 def products(request):
+    product =  Product.objects.all()
+    if 'Searchname' in request.GET:
+        name = request.GET['Searchname']
+        if name:
+            product = product.filter(name__icontains=name)
+    
+    if 'Searchdescription' in request.GET:
+        desc = request.GET['Searchdescription']
+        if desc:
+            product = product.filter(description__icontains=desc)
+
+
     items = {
-        'products' : Product.objects.all()
+        'products':product
     }
-    return render(request,'products/products.html',items )
+    return render(request, 'products/products.html', items)
 
 
+ 
 def product(request,product_id):
     item = {
         'product':get_object_or_404(Product,pk=product_id)
     }
     return render(request,'products/product.html',item)
-
 
 def search(request):
     return render(request,'products/search.html')
