b99a5f6b1fa69b3274873b4e221baa02920ffd4b
Islam412 <islam.124.hamdy@gmail.com>
2023-10-16 18:27:14 +0300

    Run search by

--- products/views.py
+++ products/views.py
@@ -4,22 +4,31 @@ from .models import Product
 
 def products(request):
     product =  Product.objects.all()
+    cs = request.GET.get('cs', 'off')
+    
     if 'Searchname' in request.GET:
         name = request.GET['Searchname']
         if name:
-            product = product.filter(name__icontains=name)
+            if cs == 'on':
+                product = product.filter(name__contains=name)
+            else:
+                product = product.filter(name__icontains=name)
     
     if 'Searchdescription' in request.GET:
         desc = request.GET['Searchdescription']
         if desc:
-            product = product.filter(description__icontains=desc)
+            if cs == 'on':
+                product = product.filter(description__contains=desc)
+            else:
+                product = product.filter(description__icontains=desc)
+
     
     if 'Searchpricefrom' in request.GET and 'Searchpriceto' in request.GET:
         price_from = request.GET['Searchpricefrom']
         price_to = request.GET['Searchpriceto']
         if price_from.isdigit() and price_to.isdigit():
             product = product.filter(price__gte=price_from,price__lte=price_to)  # get----> >= , lte----> <=
-
+    
     items = {
         'products':product
     }
