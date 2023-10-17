ac5896ecac5697612700a5587976b5517d3c725a
Islam412 <islam.124.hamdy@gmail.com>
2023-10-18 01:11:34 +0300

    call name for button in pages accounts.html

--- accounts/views.py
+++ accounts/views.py
@@ -1,19 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib import messages
 # Create your views here.
 
 
 def signin(request):
-    if request.GET:
-        messages.info(request,'testing messages 1')
-        messages.success(request,'testing messages 2')
-        messages.error(request,'testing messages 3')
-    return render(request , 'accounts/signin.html')
+    if request.method == 'POST' and 'btnlogin' in request.POST:
+        messages.info(request,'This is POST and btnlogin')
+        return redirect('signin')
+    else:
+        return render(request , 'accounts/signin.html')
 
 
 def signup(request):
-    return render(request , 'accounts/signup.html')
+    if request.method == 'POST' and 'btnsignup' in request.POST:
+        messages.info(request,'This is POST and btnsignup')
+        return redirect('signup')
+    else:
+        return render(request , 'accounts/signup.html')
 
 
 def profile(request):
-    return render(request,'accounts/profile.html')
+    if request.method == 'POST' and 'btnsave' in request.POST:
+        messages.info(request,'This is POST and btnsave')
+        return redirect('profile')
+    else:
+        return render(request,'accounts/profile.html')
