5735fd85b4466d7c7332510c45d6d3227547980e
Islam412 <islam.124.hamdy@gmail.com>
2023-10-19 00:49:33 +0300

    create new accouns and check data found in database

--- accounts/views.py
+++ accounts/views.py
@@ -17,8 +17,8 @@ def signin(request):
 def signup(request):
     if request.method == 'POST' and 'btnsignup' in request.POST:
         #variables for fieles
-        fname = None
-        lname = None
+        first_name = None
+        last_name = None
         address = None
         address2 = None
         city = None
@@ -30,9 +30,9 @@ def signup(request):
         terms = None
         
         #GET value from form
-        if 'fname' in request.POST: fname = request.POST['fname']
+        if 'first_name' in request.POST: first_name = request.POST['first_name']
         else: messages.error(request,'Error in frist name')
-        if 'lname' in request.POST: lname = request.POST['lname']
+        if 'last_name' in request.POST: last_name = request.POST['last_name']
         else: messages.error(request,'Error in last name')
         if 'address' in request.POST: address = request.POST['address']
         else: messages.error(request,'Error in  address')
@@ -42,18 +42,18 @@ def signup(request):
         else: messages.error(request,'Error in  city')
         if 'state' in request.POST: state = request.POST['state']
         else: messages.error(request,'Error in  state')
-        if 'zip' in request.POST: zip_number = request.POST['zip']
+        if 'zip_number' in request.POST: zip_number = request.POST['zip_number']
         else: messages.error(request,'Error in  zip number')
         if 'email' in request.POST: email = request.POST['email']
         else: messages.error(request,'Error in  email')
-        if 'user' in request.POST: username = request.POST['user']
+        if 'username' in request.POST: username = request.POST['username']
         else: messages.error(request,'Error in  user name')
         if 'password' in request.POST: password = request.POST['password']
         else: messages.error(request,'Error in  password')
         if 'terms' in request.POST: terms = request.POST['terms']
         
         #check all values
-        if fname and lname and address and address2 and city and state and zip_number and email and username and password:
+        if first_name and last_name and address and address2 and city and state and zip_number and email and username and password:
             if terms == 'on':
                 #check if username
                 if User.objects.filter(username=username).exists():
@@ -63,23 +63,34 @@ def signup(request):
                     if User.objects.filter(email=email).exists():
                         messages.error(request,'E-Mail is taken')
                     else:
-                        patt = "^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$"
+                        patt = "^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$" #صيغة الايميل
                         if re.match(patt,email):
                             # add User
-                            user = User.objects.create_user(first_name=fname,last_name=lname,email=email,username=username,password=password) #حقل ديجانجو = متغير الخاص بي
+                            user = User.objects.create_user(first_name=first_name,last_name=last_name,email=email,username=username,password=password) #حقل ديجانجو = متغير الخاص بي
                             user.save()
                             # add user profile
                             userprofile = UserProfile(user=user,address=address,address2=address2,city=city,state=state,zip_number=zip_number)
                             userprofile.save()
                             #Success message
-                            messages.success(request,'Youer account is crated')
+                            messages.success(request,'Your account has been created successfully')
                         else:
                             messages.error(request,'Invalid e-mail')
             else:
-                message.error(request,'You must agree to the terms')
+                messages.error(request,'You must agree to the terms')
         else:
-            messages.error(request,'check empty fields')
-        return redirect('signup')
+            messages.error(request, 'Check empty fields')
+        return render(request, 'accounts/signup.html',{
+                'first_name': first_name,
+                'last_name': last_name,
+                'address': address,
+                'address2': address2,
+                'city': city,
+                'state': state,
+                'zip_number': zip_number,
+                'email': email,
+                'user': username,
+                'password': password,
+        })
     else:
         return render(request , 'accounts/signup.html')
 
