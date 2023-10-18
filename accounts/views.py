51b1d9ad7c0dbda80bff7615e3a114d437593ec7
Islam412 <islam.124.hamdy@gmail.com>
2023-10-18 03:43:49 +0300

    desing view sign up p1

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render,redirect
 from django.contrib import messages
-# Create your views here.
+from django.contrib.auth.models import User
+from .models import UserProfile
+import re
+
 
 
 def signin(request):
@@ -13,7 +16,32 @@ def signin(request):
 
 def signup(request):
     if request.method == 'POST' and 'btnsignup' in request.POST:
-        messages.info(request,'This is POST and btnsignup')
+        #variables for fieles
+        frist_name = None
+        last_name = None
+        address = None
+        address2 = None
+        city = None
+        state = None
+        zip_number = None
+        email = None
+        username = None
+        password = None
+        terms = None
+        
+        #GET value from form
+        if 'fname' in request.POST: frist_name = request.POST['fname']
+        if 'lname' in request.POST: last_name = request.POST['lname']
+        if 'address' in request.POST: address = request.POST['address']
+        if 'address2' in request.POST: address2 = request.POST['address2']
+        if 'city' in request.POST: city = request.POST['city']
+        if 'state' in request.POST: state = request.POST['state']
+        if 'zip' in request.POST: zip_number = request.POST['zip']
+        if 'email' in request.POST: email = request.POST['email']
+        if 'user' in request.POST: username = request.POST['user']
+        if 'password' in request.POST: password = request.POST['password']
+        if 'terms' in request.POST: terms = request.POST['terms']
+
         return redirect('signup')
     else:
         return render(request , 'accounts/signup.html')
